5170d0a89bc784addac15cce7978580e359ee2e2
Afrin Turna <turna.afrin@gmail.com>
2026-02-07 00:19:09 +0200

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,4 @@
+print("Hello World!")
+print("Welcome to Python programming.")
+print("This is a simple program to demonstrate basic syntax.")
+print("You can run this code in any Python environment.")
