ab312872f4792b7ad13a7844ad0a47c4200a052e
Afrin Turna <turna.afrin@gmail.com>
2026-02-07 00:40:47 +0200

    removed line

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,1 @@
 print("Hello World!")
-print("Welcome to Python programming.")
-print("This is a simple program to demonstrate basic syntax.")
-print("You can run this code in any Python environment.")
